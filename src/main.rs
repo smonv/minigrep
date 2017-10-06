64d8421c290a6caf1eb1f598f6ae483efd317723
Tien Thanh <tthanh.smt@gmail.com>
2017-10-06 14:04:09 +0700

    Update code use Iterators

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ extern crate minigrep;
 
 use std::env;
 use std::process;
+use std::io::Write;
 
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        writeln!(&mut stderr, "Problem parsing arguments: {}", err)
+            .expect("Counld not write to stderr");
         process::exit(1);
     });
 
